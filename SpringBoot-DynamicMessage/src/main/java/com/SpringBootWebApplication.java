6e49252913b4a6409c80d5680e05b512d79b749b
Vivek Rathod <vivekrathod9@github.com>
2019-04-08 23:32:09 +0530

    Vivek's  first commit

--- SpringBoot-DynamicMessage/src/main/java/com/SpringBootWebApplication.java
+++ SpringBoot-DynamicMessage/src/main/java/com/SpringBootWebApplication.java
@@ -13,6 +13,11 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 		return application.sources(SpringBootWebApplication.class);
 	}
 
+	
+	
+	
+	
+	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
